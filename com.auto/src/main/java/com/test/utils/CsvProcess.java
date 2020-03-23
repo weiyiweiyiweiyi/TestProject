74a470cd0f96624ec3fe8f155c7ba0c88a58c579
“weiyi” <weiyi@weiyideiMac.local>
2020-03-23 17:54:53 +0800

    modify

--- com.auto/src/main/java/com/test/utils/CsvProcess.java
+++ com.auto/src/main/java/com/test/utils/CsvProcess.java
@@ -1,20 +1,39 @@
 package com.test.utils;
 
-import java.io.FileInputStream;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+
+import com.csvreader.CsvReader;
 
 public class CsvProcess {
-	public Object[][] processCsv(String path){
-		FileInputStream file= new FileInputStream(path);
-		byte[] buffer = new byte[file.available()];
-		int len;
-		file
-		while((len = file.read(buffer))!=-1) {
-			
+	
+	public static Object[][] readCsv(String path){
+		try {
+		ArrayList<String[]> csvFile = new ArrayList<String[]>();
+		CsvReader reader = new CsvReader(path,',',Charset.forName("utf-8"));
+		reader.readHeaders();
+		while(reader.readRecord()) {
+			System.out.println(reader.getRawRecord());
+			csvFile.add(reader.getValues());
+		}
+		reader.close();
+		
+		Object[][] result = new Object[csvFile.size()][csvFile.get(0).length];
+		for(int row = 0;row<csvFile.size();row++) {
+			result[row] = csvFile.get(row);
 		}
 		
-		
+		return result;
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
 		return null;
 	} 
 	
-
+	public static void main(String[] args) {
+		readCsv("/Users/weiyi/eclipse-workspace/test2/com.auto/src/test/resources/Case.csv");
+	}
 }
