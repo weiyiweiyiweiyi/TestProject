a1166d2d26c409b72e6d53dbdf7d5bc1447de4dc
lubin <lubin@example.com>
2020-04-01 23:39:01 +0800

    modify

--- com.auto/src/test/java/com/test/api/testGet.java
+++ com.auto/src/test/java/com/test/api/testGet.java
@@ -2,7 +2,6 @@ package com.test.api;
 
 import java.io.IOException;
 
-import org.apache.http.client.ClientProtocolException;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
