fff52a0c6f9399a3f8a13ae61ec32eedfa30db90
“weiyi” <weiyi@weiyideiMac.local>
2020-04-03 14:43:39 +0800

    m

---
+++ com.auto/src/main/java/com/test/sample/GetJavaSample.java
@@ -0,0 +1,14 @@
+package com.test.sample;
+
+import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+import org.apache.jmeter.samplers.SampleResult;
+
+public class GetJavaSample extends AbstractJavaSamplerClient{
+
+	public SampleResult runTest(JavaSamplerContext arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
